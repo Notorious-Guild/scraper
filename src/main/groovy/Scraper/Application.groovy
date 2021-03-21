7fa39f49587029495cac095a339a61dc4b3138b4
Algor <algorgaming@gmail.com>
2021-03-20 22:42:30 -0300

    Don't need to Define ScrapeResource

--- src/main/groovy/Scraper/Application.groovy
+++ src/main/groovy/Scraper/Application.groovy
@@ -6,7 +6,7 @@ import groovy.util.logging.Slf4j
 class Application {
   static void main(String[] args) {
     do {
-      def ignored = new ScrapeResource()
+      new ScrapeResource()
       def frequency = 1000 * (System.getenv("FREQUENCY") as Integer)
       Thread.sleep(frequency)
     } while (true)
